97a81a981fc25a7283479545c7e59f3ff9cf2705
SatellaSoft <satellasoft@gmail.com>
2023-10-18 17:04:28 -0300

    feat: add cov

--- src/app/app.py
+++ src/app/app.py
@@ -3,5 +3,5 @@ class App:
     def sum(self, num1, num2):
         return num1 +  num2
     
-    def multiply(self, num1, num2):
+    def multiply(self, num1, num2): # pragma: no cover
         return num1 *  num2
--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
-from app.app import App
+if __name__ == "__main__": # pragma: no cover
+    from app.app import App
+    
+    app = App()
 
-app = App()
-
-print(app.sum(2, 2))
+    print(app.sum(2, 2))
